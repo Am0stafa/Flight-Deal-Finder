915f14dac5375202cd7bacddd54bfb18d0a5a6e8
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-09-28 01:35:14 +0200

    getting all the flightes

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,32 @@
+import requests
+from flight_search import FlightSearch
+    
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self,api):
+        self.api = api
+        self.sheetData = {}
+        
+    def populateIATA(self):
+        x = requests.get(self.api)
+        self.sheetData  = x.json()["prices"]
+        flightSearch = FlightSearch()
+        for i in self.sheetData:
+            if len(i["iataCode"]) == 0:
+                iata = flightSearch.getDestinationCode(i["city"])
+                i["iataCode"] = iata
+                id = i["id"]
+                url = f"https://api.sheety.co/21d95537cb39f8e2e273584f8133a12c/copyOfFlightDeals/prices/{id}"
+                requests.put(url, data = i)
+                new_data = {
+                    "price": {
+                        "iataCode": i["iataCode"]
+                    }
+                }
+                response = requests.put(
+                    url=url,
+                    json=new_data
+                )
+
+                    
+                
+        return self.sheetData         
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+data = DataManager('https://api.sheety.co/21d95537cb39f8e2e273584f8133a12c/copyOfFlightDeals/prices')
+flight = FlightSearch()
+
+ORIGIN_CITY_IATA = "LON"
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30)) #* search flights upto this date 
+
+sheetData = data.populateIATA()
+
+for i in sheetData:
+    flight.checkFlights(ORIGIN_CITY_IATA, i['iataCode'], from_time=tomorrow, to_time=six_month_from_today)
